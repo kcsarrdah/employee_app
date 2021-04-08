d1adc19c93a1c90a93487787f340184080b3d67e
HusainNahar <hussainburhanuddin1@gmail.com>
2021-04-08 21:33:07 +0530

    :

---
+++ src/components/cards/BalanceCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const BalanceCard = () => {
+  return (
+    <div class="card text-center bg-gray-50 py-6 px-auto border-black border-rounded ml-4">
+      <div class="card-header h-1/2 w-auto">
+        <div class="font-bold text-gray-90 p-2">Point balance</div>
+      </div>
+      <div class="flex items-center text-xl justify-center p-3 space-x-3 border-t-2">
+        Balance
+      </div>
+    </div>
+  );
+};
+
+export default BalanceCard;
--- src/pages/EmployeeDashboard.jsx
+++ src/pages/EmployeeDashboard.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import BalanceCard from "../components/cards/BalanceCard";
+import MainBody from "../components/headers/MainBody";
+import ProfileDisplay from "../components/profile/profileDisplay";
 
 const EmployeeDashboard = () => {
-  return <h1>EmployeeDashboard</h1>;
+  return (
+    <MainBody>
+      <div className="grid grid-cols-4 gap-4 ">
+        <div className="col-span-3 flex">
+          <ProfileDisplay />
+          <BalanceCard />
+        </div>
+        <div className="bg-blue-300">2</div>
+      </div>
+    </MainBody>
+  );
 };
 
 export default EmployeeDashboard;
