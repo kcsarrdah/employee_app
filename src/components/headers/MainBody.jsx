9d8666161227ff2c6b3d139c452298a40227ea04
Krishnna <kcsarrdah@gmail.com>
2021-04-09 00:21:14 +0530

    footer added special perks added

--- src/components/headers/MainBody.jsx
+++ src/components/headers/MainBody.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import Footer from "../footer/footer";
 import Header from "./Header";
 
 const MainBody = (props) => {
   return (
     <div id="mainPage" className="bg-purple-100 h-full">
       <Header />
-      <section class="text-gray-600 body-font ">
+      <section class="text-gray-600 body-font">
         <div class="container px-2 pt-12 pb-2 mx-auto">{props.children}</div>
       </section>
+      <Footer />
     </div>
   );
 };
