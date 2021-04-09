60d5e7290677677b1dd079a7087270410d2d5e32
HusainNahar <hussainburhanuddin1@gmail.com>
2021-04-09 19:51:26 +0530

    BGColor Added

--- src/components/headers/MainBody.jsx
+++ src/components/headers/MainBody.jsx
@@ -4,7 +4,10 @@ import Header from "./Header";
 
 const MainBody = (props) => {
   return (
-    <div id="mainPage" className="bg-yellow-100 h-full">
+    <div
+      id="mainPage"
+      className="h-full bg-gradient-to-r from-blue-300 via-green-500 to-purple-500"
+    >
       <Header />
       <section class="text-gray-600 body-font">
         <div class="container px-2 pt-12 pb-2 mx-auto">{props.children}</div>
