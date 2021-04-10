f21cb6c6fb06427437eac63d6d43e4ba9b706652
Krishnna <kcsarrdah@gmail.com>
2021-04-10 17:35:18 +0530

    employee details added

--- src/pages/employeeInfo.jsx
+++ src/pages/employeeInfo.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import DataDisplay from '../components/DataDisplay/datadisplay'
+import MainBody from '../components/headers/MainBody';
 import ProfileDisplay from '../components/profile/profileDisplay';
 
 
 const EmployeeDetails = () => {
-    return ( 
-        <div>
-            <ProfileDisplay />
-            <DataDisplay />
-        </div>
-     );
+    return (
+    <MainBody>
+        <DataDisplay />
+    </MainBody>
+
+    );
 }
- 
+
 export default EmployeeDetails;
